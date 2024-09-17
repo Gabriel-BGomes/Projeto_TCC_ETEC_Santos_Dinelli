a27375ca4134b2053908aea923bcb1ecf93cd7c1
matheushahah <matheusoliveirale2007@gmail.com>
2024-09-17 10:46:29 -0300

    tela-inicial-pronta

--- js/mensal.js
+++ js/mensal.js
@@ -1,74 +1,104 @@
 const mensal = document.getElementById('mensal'); // é o id puxado do html para fazer tudo aconter
+const mesAtual = new Date().getMonth()
 
-let mesAtual = new Date().getMonth()+1
 
-let meses = {
-  1: ["JANEIRO"],
-  2: ["FEVEREIRO"],
-  3: ["MARÇO"],
-  4: ["ABRIL"],
-  5: ["MAIO"],
-  6: ["JUNHO"],
-  7: ["JULHO"],
-  8: ["AGOSTO"],
-  9: ["SETEMBRO"],
-  10: ["OUTUBRO"],
-  11: ["NOVEMBRO"],
-  12: ["DEZEMBRO"],
+const meses = {
+  0: 'Janeiro',
+  1: 'Fevereiro',
+  2: 'Março',
+  3: 'Abril',
+  4: 'Maio',
+  5: 'Junho',
+  6: 'Julho',
+  7: 'Agosto',
+  8: 'Setembro',
+  9: 'Outubro',
+  10: 'Novembro',
+  11: 'Dezembro',
 }
 
+const mesSelecionadoRecebimento = {
+  'Janeiro': 0,
+  'Fevereiro': 0,
+  'Março': 0,
+  'Abril': 0,
+  'Maio': 0, 
+  'Junho': 0,
+  'Julho': 0,
+  'Agosto': 0,
+  'Setembro': 20,
+  'Outubro': 0,
+  'Novembro': 0,
+  'Dezembro': 0
+};
+
+const mesSelecionadoDespesa = {
+  'Janeiro': 0,
+  'Fevereiro': 0,
+  'Março': 0,
+  'Abril': 0,
+  'Maio': 0, 
+  'Junho': 0,
+  'Julho': 0,
+  'Agosto': 0,
+  'Setembro': 30,
+  'Outubro': 0,
+  'Novembro': 0,
+  'Dezembro': 0
+};
+
+const anual = {
+  mesSelecionadoRecebimento,
+  mesSelecionadoDespesa
+}
+
+const mesAtualString = meses[mesAtual]
+
+
+
+// da mesma forma como pode ser criada uma vaiável no data, você pode criar uma varíavel para definir o text de todas as fontes
+const fonte = {
+  family: 'Times New Roman',
+  size: 55,
+}
+
+
 // aqui você pode declarar uma variável ou declarar no próprio chart as informações do gráfico.
-var dataMensal = {
+const dataMensal = {
   labels: ['RESUMO'], // colunas
   datasets: [{
     label: 'RECEBIMENTOS', // nome da primeira coluna
-    data: [20], // dados da primeira coluna
+    data: [mesSelecionadoRecebimento[mesAtualString]], // dados da primeira coluna
     backgroundColor: 'green' // cor da primeira coluna
   },
 
   {
     label: 'DESPESAS', // nome da segunda coluna
-    data: [10], // dados da segunda coluna
+    data: [mesSelecionadoDespesa[mesAtualString]], // dados da segunda coluna
     backgroundColor: 'red' // cor da segunda coluna
   }
 ]
 };
 
-
-// da mesma forma como pode ser criada uma vaiável no data, você pode criar uma varíavel para definir o text de todas as fontes
-var fonte = {
-  family: 'Times New Roman',
-  size: 55,
-  color: 'black'
-}
-
-var options = {
-
-  responsive: true,
-  maintainAspectRadio: true,
-
-  animations: { // opções de animação caso o type for "line"
-    tension: {
-      duration: 500,
-      easing: 'easeInOutBounce',
-      from: 1,
-      to: 0,
-      loop: true
-    }
-    
-  },
-
-  scales: { // scales pelo que entendi irá mexer no nome dos índices
+const options = {
+    responsive: true,
+    maintainAspectRadio: false,
+    responsive: true,
+    scales: { // scales pelo que entendi irá mexer no nome dos índices
     x: {
       ticks: {
         color: 'black', // cor da legenda X
-      }
+      },
+
     },
 
     y: { // defining min and max so hiding the d,ataset does not change scale range
       ticks: {
         color: 'black' // cor da legenda Y
-      }
+      },
+
+      
+
     }
 
   },
@@ -76,30 +106,24 @@ var options = {
   plugins: { // para fazer mais configurações de estilo
     title: { // adicionar um estilo ao gráfico
       display: true,
-      text: `RESUMO MENSAL DE ${meses[mesAtual]}`, // título
+      text: `RESUMO MENSAL DE ${meses[mesAtual].toUpperCase()}`, // título
       font: fonte, // puxando a constante de fonte
-      color: 'purple' // mudando a cor do título
+      color: '#145400' // mudando a cor do título
     },
-
+    
     tooltip: { // tooltip é o que aparece quando o mouse fica em cima de cada coluna
       titleColor: 'pink', // mudando a cor do título da coluna
       bodyColor: 'white', // mudando a cor da legenda do pop-up
       backgroundColor: 'rgb(60, 66, 71)' // mudando o background do pop-up
     },
 
-    legend: {
-      labels: {
-        color: 'red' // aqui irá mudar a cor das legendas de cada coluna (verificar se têm como mudar indiviudalmente cada coluna)
-      }
-    }
+    // aqui irá mudar a cor das legendas de cada coluna (verificar se têm como mudar indiviudalmente cada coluna)
 
   }
 }
 
-// definindo o background padrão
-Chart.defaults.backgroundColor = 'pink';
 
-var definingChart = {
+const definingChart = {
   type: 'bar', // tipo do gráfico (linha, coluna)
   data: dataMensal, // puxando a constante data
   options: options
