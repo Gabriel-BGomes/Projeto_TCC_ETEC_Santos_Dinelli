89efc2be0fb7a58afebe9f7661f3bd0ce9ad18d4
matheushahah <matheusoliveirale2007@gmail.com>
2024-10-02 22:47:37 -0300

    fazendo alterações

--- src/php/agenda/cadastrar_evento.php
+++ src/php/agenda/cadastrar_evento.php
@@ -7,10 +7,11 @@ include_once './conexao.php';
 $dados = filter_input_array(INPUT_POST, FILTER_DEFAULT);
 
 // query cadastrar no banco
-$query_cad_event = "INSERT INTO events (title, color, start, end, obs) VALUES (:title, :color, :start, :end, :obs)";
+$query_cad_event = "INSERT INTO events (title, color, start, end, obs, servico) VALUES (:title, :color, :start, :end, :obs, :servico)";
 
 // prepara a QUERY
 $cad_event = $conn->prepare($query_cad_event);
+
 
 // substituir o link pelo valor
 $cad_event->bindParam(':title', $dados['cad_title']);
@@ -18,10 +19,11 @@ $cad_event->bindParam(':color', $dados['cad_color']);
 $cad_event->bindParam(':start', $dados['cad_start']);
 $cad_event->bindParam(':end', $dados['cad_end']);
 $cad_event->bindParam(':obs', $dados['cad_obs']);
+$cad_event->bindParam(':servico', $dados['cad_servico']);
 
 // verificar se consegui cadastrar corretamente
 if ($cad_event->execute()) {
-    $retorna = ['status' => true, 'msg' => 'Evento cadastrado com sucesso!', 'id' => $conn->lastInsertId(), 'title' => $dados['cad_title'], 'color' => $dados['cad_color'], 'start' => $dados['cad_start'], 'end' => $dados['cad_end'], 'obs' => $dados['cad_obs']];
+    $retorna = ['status' => true, 'msg' => 'Evento cadastrado com sucesso!', 'id' => $conn->lastInsertId(), 'title' => $dados['cad_title'], 'color' => $dados['cad_color'], 'start' => $dados['cad_start'], 'end' => $dados['cad_end'], 'obs' => $dados['cad_obs'], 'servico' => $dados['cad_servico']];
 } else {
     $retorna = ['status' => false, 'msg' => 'Erro: Evento não cadastrado!'];
 }
